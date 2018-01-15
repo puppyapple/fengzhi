61b48f658f41cb9c5ad01f295ac72391f1d5424d
Alexandre <puppyapplewzj@126.com>
2018-01-15 23:52:49 +0800

    sfs

--- fengzhi.py
+++ fengzhi.py
@@ -4,11 +4,14 @@ import numpy as np
 import sys
 
 def grow_factor(end_year, season, num_year):
+    '''
+    计算成长因子
+    '''
     # 成长因子：主营业务三年复合增长率，净利润三年复合增长率，内部收益率
-    df_profit_old = ts.get_profit_data(end_year-num_year,season)[["name","code","roe","business_income","net_profits"]]
-    df_profit_new = ts.get_profit_data(end_year,season)[["name","code","roe","business_income","net_profits"]]
-    df_profit_old.columns = ["name","code","roe_old","business_income_old","net_profits_old"]
-    df_profit_new.columns = ["name","code","roe_new","business_income_new","net_profits_new"]
+    df_profit_old = ts.get_profit_data(end_year-num_year, season)[["name", "code", "roe", "business_income", "net_profits"]]
+    df_profit_new = ts.get_profit_data(end_year, season)[["name", "code", "roe", "business_income", "net_profits"]]
+    df_profit_old.columns = ["name", "code", "roe_old", "business_income_old", "net_profits_old"]
+    df_profit_new.columns = ["name", "code", "roe_new", "business_income_new", "net_profits_new"]
 
     data = pd.merge(df_profit_new, df_profit_old, how='inner')
 
@@ -22,12 +25,15 @@ def grow_factor(end_year, season, num_year):
     data["roe_rate"] = (data["roe_new"]/data["roe_old"])**(1.0/num_year) - 1
     data["bi_rate"] = (data["business_income_new"]/data["business_income_old"])**(1.0/num_year) - 1
     data["ne_rate"] = (data["net_profits_new"]/data["net_profits_old"])**(1.0/num_year) - 1
-    return data[["name","code","roe_rate","bi_rate","ne_rate"]]
+    return data[["name", "code", "roe_rate", "bi_rate", "ne_rate"]]
 
 def value_factor(end_year, season):
+    '''
+    计算价值因子
+    '''
     #价值因子：每股收益与价格比率、每股经营现金流与价格比率、每股净资产与价格比率、股息收益率
-    df_report_new = ts.get_profit_data(end_year,season)[["name","code","esp","epcf","bvps"]]
-    df_new_price = ts.get_today_all()[["name","code","close"]]
+    df_report_new = ts.get_profit_data(end_year, season)[["name", "code", "esp", "epcf", "bvps"]]
+    df_new_price = ts.get_today_all()[["name", "code", "close"]]
 
     data = pd.merge(df_report_new, df_new_price, how='inner')
 
@@ -37,7 +43,13 @@ def value_factor(end_year, season):
     data["esp_rate"] = data["esp"]/data["close"]
     data["epcf_rate"] = data["epcf"]/data["close"]
     data["bvps_rate"] = data["bvps"]/data["close"]
-    return data[["name","code","esp_rate","epcf_rate","bvps_rate","interest_rate"]]
+    return data[["name", "code", "esp_rate", "epcf_rate", "bvps_rate"]]
 
 def process_data(df_grow, df_value):
-    data = pd.merge(df_grow, df_value, how='inner')[["name","code","roe_rate","bi_rate","ne_rate","esp_rate","epcf_rate","bvps_rate","interest_rate"]]
+    '''
+    合并因子数据并作处理
+    '''
+    data = pd.merge(df_grow, df_value, how='inner')
+    df_index = data[["code", "name"]]
+    df_factor = data[["roe_rate", "bi_rate", "ne_rate", "esp_rate", "epcf_rate", "bvps_rate"]]
+    np.percentile(df_factor, 95)
